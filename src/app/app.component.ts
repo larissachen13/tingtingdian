c762150c0107b1574f0eb3b1361f0823ac97d616
Larissa Chen <larissajchen@google.com>
2024-10-13 21:24:21 -0400

    changed angular.json to not output browser subdir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Default</h1>`,
-  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'default';
+  title = 'tingtingdian';
 }
